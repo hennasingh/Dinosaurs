7ebde3293b67e70ac0a7ed1f436848f1db617b5a
hennasingh <hennasingh04@gmail.com>
2021-01-04 20:57:42 +0000

    Created 8 Dino objects from the Constructor Function

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 
     // Create Dino Constructor
     function Dinosaurs(species, weight, height,diet, where, when, fact ) {
-      this.species = species,
-      this.weight = weight,
-      this.height = height,
-      this.diet = diet,
-      this.where = where,
-      this.when = when,
-      this.fact = fact
+      this.species = species;
+      this.weight = weight;
+      this.height = height;
+      this.diet = diet;
+      this.where = where;
+      this.when = when;
+      this.fact = fact;
     }
 
    // Create Dino Objects
+
+   //Dino 1
+   const Triceratops = new Dinosaurs("Triceratops", 13000, 114, "herbavor", "North America",
+   "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh");
+
+   //Dino 2
+   const Tyrannosaurus = new Dinosaurs("Tyrannosaurus Rex", 11905, 144, "carnivor", "North America",
+   "Late Cretaceous", "The largest known skull measures in at 5 feet long");
+
+  //Dino 3
+  const Anklyosaurus = new Dinosaurs("Anklyosaurus", 10500, 55, "herbavor", "North America", "Late Cretaceous",
+  "Anklyosaurus survived for approximately 135 million years");
+
+  //Dino 4
+  const Brachiosaurus = new Dinosaurs("Brachiosaurus", 70000, 372, "herbavor", "North America", "Late Jurassic",
+   "An asteroid was named 9954 Brachiosaurus in 1991");
+
+   //Dino 5
+   const Stegosaurus = new Dinosaurs("Stegosaurus", 11600, 79, "herbavor", "North America, Europe, Asia",
+    "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines");
+
+  //Dino 6
+  const Elasmosaurus = new Dinosaurs("Elasmosaurus", 16000, 59, "carnivor","North America", "Late Cretaceous",
+  "Elasmosaurus was a marine reptile first discovered in Kansas");
+
+  //Dino 7
+  const Pteranodon = new Dinosaurs("Pteranodon", 44, 20, "carnivor", "North America", "Late Cretaceous",
+  "Actually a flying reptile, the Pteranodon is not a dinosaur");
+
+  //Dino 8
+  const Pigeon = new Dinosaurs("Pigeon", 0.5, 9, "herbavor", "World Wide", "Holocene", "All birds are living dinosaurs")
 
 
     // Create Human Object
